3721fefdf258c4c23c44a331d4f68fe610312c2e
Eric Wooley <ericwooley@gmail.com>
2021-10-27 16:35:55 -0700

    wip

--- test-rust/src/lib.rs
+++ test-rust/src/lib.rs
@@ -9,7 +9,24 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-pub fn greet() -> &'static str {
-    let greeting = "Hello";
-    return greeting;
+extern "C" {
+    // Use `js_namespace` here to bind `console.log(..)` instead of just
+    // `log(..)`
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+
+    // The `console.log` is quite polymorphic, so we can bind it with multiple
+    // signatures. Note that we need to use `js_name` to ensure we always call
+    // `log` in JS.
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_u32(a: u32);
+
+    // Multiple arguments too!
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_many(a: &str, b: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    log("Hello, world!");
 }
